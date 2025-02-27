e433c5bc3374aba6625ad479cd9cf56dd4e0a61b
PinchToDebug <52333417+PinchToDebug@users.noreply.github.com>
2025-02-27 17:30:56 +0100

    Update App.xaml.cs toast action

--- Pihole_Tray/App.xaml.cs
+++ Pihole_Tray/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,23 @@ namespace Pihole_Tray
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ToastNotificationManagerCompat.OnActivated += ToastActivatedHandler;
+        }
+        private void ToastActivatedHandler(ToastNotificationActivatedEventArgsCompat toastArgs)
+        {
+            var args = ToastArguments.Parse(toastArgs.Argument);
+            Current.Dispatcher.Invoke(() =>
+            {
+                if (args.Contains("action") && args["action"] == "install_update")
+                {
+                    Updater.InstallUpdate();
+                }
+
+            });
+        }
     }
 
 }
